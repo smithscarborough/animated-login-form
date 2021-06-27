c2335c64c4a95cb37d4e217570326ff27dd68280
Smith Scarborough <smithscarborough@gmail.com>
2021-06-26 21:42:19 -0500

    initial repo w/ basic form, styling

---
+++ src/App.svelte
@@ -0,0 +1,62 @@
+<script>
+
+</script>
+
+<style>
+	form {
+		--text-color: #afafaf;
+		max-width: 500px;
+	}
+
+	.field {
+		width: 100%;
+		position: relative;
+		border-bottom: 2px dashed var(--text-color);
+		margin: 4rem auto 1rem;
+	}
+
+	.label {
+		color: var(--text-color);
+		font-size: 1.2rem;
+	}
+
+	.input {
+		outline: none;
+		border: none;
+		overflow: hidden;
+		margin: 0;
+		width: 100%;
+		padding: 0.25rem 0;
+		background: none;
+		color: white;
+		font-size: 1.2rem;
+		font-weight: bold;
+	}
+
+	.input:valid {
+		color: yellowgreen;
+	}
+
+	.input:invalid {
+		color: orangered;
+	}
+
+</style>
+
+<main>
+	<form>
+
+	 
+		<div class="field">
+			<input type="email" name="email" class="input" placeholder="" />
+			<label for="email" class="label">Email</label>
+		</div>
+
+		<div class="field">
+			<input type="password" class="input" placeholder="" />
+			<label for="password" class="label">Password</label>
+		</div>
+
+
+	</form>
+</main>
