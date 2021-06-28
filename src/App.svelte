b0c55d24c4c4a864734a0e78c28da4b5f395d1f0
Smith Scarborough <smithscarborough@gmail.com>
2021-06-27 20:25:18 -0500

    add password strength bar, form input css styling

--- src/App.svelte
+++ src/App.svelte
@@ -41,6 +41,77 @@
 		color: orangered;
 	}
 
+	/* Border animation */
+	.field::after {
+		content: '';
+		position: relative;
+		display: block;
+		height: 4px;
+		width: 100%;
+		background: #d16dff;
+		transform: scaleX(0);
+		transform-origin: 0%;
+		transition: transform 500ms ease;
+		top: 2px;
+	}
+
+	.field:focus-within {
+		border-color: transparent;
+	}
+
+	.field:focus-within::after {
+		transform: scaleX(1);
+	}
+
+	/* label animation */
+	.label {
+		z-index: -1;
+		position: absolute;
+		transform: translateY(-2rem);
+		transform-origin: 0%; 
+		transition: transform 400ms;
+	}
+
+	.field:focus-within .label,
+	.input:not(:placeholder-shown) + .label{
+		transform: scale(0.8) translateY(-5rem);
+	}
+
+/* password strength meter */
+	.strength {
+		display: flex;
+		height: 20px;
+		width: 100%;
+	}
+
+	.bar {
+		margin-right: 5px;
+		height: 100%;
+		width: 25%;
+		transition: box-shadow 500ms;
+		box-shadow: inset 0px 20px #1f1f1f;
+	}
+
+	.bar-show {
+		box-shadow: none;
+	}
+
+	.bar-1 {
+		background: linear-gradient(to right, red, orangered);
+	}
+
+	.bar-2 {
+		background: linear-gradient(to right, orangered, yellow);
+	}
+
+	.bar-3 {
+		background: linear-gradient(to right, yellow, yellowgreen);
+	}
+
+	.bar-4 {
+		background: linear-gradient(to right, yellowgreen, green);
+	}
+
 </style>
 
 <main>
@@ -56,6 +127,20 @@
 			<input type="password" class="input" placeholder="" />
 			<label for="password" class="label">Password</label>
 		</div>
+
+		<div class="strength">
+			<span class="bar bar-1" />
+			<span class="bar bar-2" />
+			<span class="bar bar-3" />
+			<span class="bar bar-4" />
+		</div>
+
+		<ul>
+			<li> must be at least 5 characters</li>
+			<li> must contain a capital letter</li>
+			<li> must contain a number</li>
+			<li> must contain one of $&+,:;=?@#</li>
+		</ul>
 
 
 	</form>
